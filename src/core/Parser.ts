bbefeae3325e4b33f7746e7a5170f5114dace9e0
Ezequiel Barbero <barbero.oe@gmail.com>
2021-09-26 21:04:20 -0300

    Supports primitive booleans

--- src/core/Parser.ts
+++ src/core/Parser.ts
@@ -1,5 +1,6 @@
 import { Expr, Literal } from './Expr'
 import { Token } from './Token'
+import { TokenType } from './TokenType'
 
 export class Parser {
   private current = 0
@@ -10,9 +11,21 @@ export class Parser {
     return this.primary()
   }
 
-  private primary() {
+  private primary(): Expr {
+    if (this.match(TokenType.TRUE)) return new Literal(true)
+    if (this.match(TokenType.FALSE)) return new Literal(false)
     return new Literal(this.peek().literal)
   }
+
+  private match(...args: TokenType[]): boolean {
+    if (args.includes(this.peek().type)) {
+      this.advance()
+      return true
+    }
+    return false
+  }
+
+  private advance = () => this.current++
 
   private peek(): Token {
     const token = this.tokens[this.current]
